6aa245ca570c90d85f21323327b3184bd8a19312
mirzamahmudov007 <gayratjonuzbek07@gmail.com>
2024-12-18 12:25:40 +0500

    created

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,15 +1,31 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.entity.User;
+import com.example.demo.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
 
+    @Autowired
+    private  UserRepo userRepo;
+
     @GetMapping("/test")
     public String test() {
         return "test";
+    }
+
+    @PostMapping("/create")
+    public User createUser(@RequestBody User user) {
+        User user1 = userRepo.save(user);
+        return user1;
+    }
+
+    @GetMapping("/getAll")
+    public List<User> getAll(){
+        return userRepo.findAll();
     }
 }
---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,19 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    private String username;
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
